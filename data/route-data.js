b6982d01dd6d2dc30a4a19967bd67675ded78105
joostbrinkman <joost.brinkman@me.com>
2025-02-15 18:12:05 +0100

    Update route-data.js

--- data/route-data.js
+++ data/route-data.js
@@ -419,8 +419,8 @@ const ROUTE_DATA = {
             },
             postcard: {
                 id: "postcard15",
-                image: "img/placeholder_thumb.png",
-                large_image: "img/placeholder.png",
+                image: "img/MonacoCasino.jpg",
+                large_image: "img/MonacoCasino.jpg",
                 support_image: "img/Monaco Casino.jpg",
                 support_image_source: "",
                 name: "Monaco Casino",
